f3543df562668bd64d29a42544cf09b1d03d1032
李勇 <yong.li@xiaomicp.com>
2020-06-23 18:48:19 +0800

    2323

---
+++ 15 MyBatis/demo/mybatis-basic/src/main/java/com/yong/mybatis/basic/entity/UserInfo.java
@@ -0,0 +1,32 @@
+package com.yong.mybatis.basic.entity;
+
+public class UserInfo {
+	private Long userId;
+	private String username;
+	private String password;
+	private Integer age;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+}
