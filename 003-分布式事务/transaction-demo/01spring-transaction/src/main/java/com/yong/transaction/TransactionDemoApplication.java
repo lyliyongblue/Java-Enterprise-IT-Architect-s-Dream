add4b0a54072ec33685fbe8981da07c07edf3701
李勇 <yong.li@xiaomicp.com>
2020-08-06 21:40:35 +0800

    ss

---
+++ 003-分布式事务/transaction-demo/01spring-transaction/src/main/java/com/yong/transaction/TransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yong.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionDemoApplication.class, args);
+    }
+
+}
---
+++ 003-分布式事务/transaction-demo/01spring-transaction/src/test/java/com/yong/transaction/TransactionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
