e575437142c0bea9a79673bae7b33dc5a2930cfa
李勇 <yong.li@xiaomicp.com>
2020-08-21 12:11:57 +0800

    11

---
+++ 017-Dubbo/demo/mirco-docker/course-edge-api/src/main/java/com/yong/course/edge/api/CourseEdgeApiApplication.java
@@ -0,0 +1,11 @@
+package com.yong.course.edge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEdgeApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEdgeApiApplication.class, args);
+	}
+}
---
+++ 017-Dubbo/demo/mirco-docker/dubbo/course-service-provider/src/main/java/com/yong/course/provider/IHelloService.java
@@ -0,0 +1,5 @@
+package com.yong.course.provider;
+
+public interface IHelloService {
+	String sayHello(String name);
+}
